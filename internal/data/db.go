59b2c538d5dafed34c8dc42fc0d1068fe3f6c966
Johan van Rhyn <johan@squarehole.dev>
2023-10-14 17:33:15 +0200

    Add database initialization and migration functionality
    - Added `InitDatabase` function to initialize the database connection and perform migrations.
    - Retrieved the database connection string from the environment.
    - Logged the database initialization.
    - Opened a connection to the database using the specified connection string.
    - Logged any errors that occurred during the connection initialization.
    - Performed database migrations for specified models.
    - Logged any errors that occurred during the migrations.
    
    Retrieve all UrlConfig objects from the database
    
    - Added `GetAllConfigs` function to retrieve all UrlConfig objects from the database.
    - Queried the database for all UrlConfig objects.
    - Logged any errors that occurred during the query.
    - Logged the number of UrlConfig objects retrieved.

--- internal/data/db.go
+++ internal/data/db.go
@@ -10,21 +10,30 @@ import (
 	"gorm.io/gorm"
 )
 
+// db is a global variable that holds the database connection
 var db *gorm.DB
 var err error
 
+// InitDatabase initializes the database connection and performs migrations
 func InitDatabase() error {
+	// Get the database connection string from the environment
 	dsn := os.Getenv("CONNECTION")
+
+	// Log the database initialization
 	slog.Info("Initializing database", "connection", dsn)
 
+	// Open a connection to the database
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
+		// Log the error if the connection fails
 		slog.Error(err.Error())
 		return err
 	}
 
+	// Perform database migrations
 	err = db.AutoMigrate(&models.UrlConfig{}, &models.Result{}, &models.Summary{}, &models.TestStatus{})
 	if err != nil {
+		// Log the error if migrations fail
 		slog.Error(err.Error())
 		return err
 	}
@@ -32,14 +41,19 @@ func InitDatabase() error {
 	return nil
 }
 
+// GetAllConfigs retrieves all UrlConfig objects from the database
 func GetAllConfigs() ([]models.UrlConfig, error) {
 	var configs []models.UrlConfig
+
+	// Query the database for all UrlConfig objects
 	err := db.Find(&configs).Error
 	if err != nil {
+		// Log the error if the query fails
 		slog.Error(err.Error())
 		return nil, err
 	}
 
+	// Log the number of UrlConfig objects retrieved
 	slog.Info("GetAllConfigs", "count", len(configs))
 	return configs, nil
 }
